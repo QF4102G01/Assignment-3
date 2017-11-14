3cda63182944fb6b8af9cbd6d14108793ea431b3
wangzexin <wang.zexin@dhs.sg>
2017-11-14 09:19:55 +0800

    Add code for Q1 (PSOR not completed yet)

---
+++ Main/FD_ids_Acall_trans.m
@@ -0,0 +1,44 @@
+function OptVal = FD_ids_Acall_trans(S0, X, r, q, T, sigma, I, N, xmax, epsilon, omega)
+	deltaT = T / N;
+	deltaX = xmax / I;
+    alpha = sigma ^ 2 * deltaT / (deltaX ^ 2);
+    beta = 1 + r * deltaT;
+    gamma = deltaT * (r - q - sigma ^ 2 / 2) / (2 * deltaX);
+	a = gamma - alpha / 2;
+	b = beta + alpha;
+	c = -gamma - alpha / 2;
+	
+    optionPayoffs = transpose(max(exp(deltaX * (-I+1 : I-1)) - X, 0));
+	U = optionPayoffs;
+	
+	for j = N-1 : -1 : 0
+		U = PSOR(a, b, c, U, U, epsilon, omega, optionPayoffs, 2*I-1);
+	end
+	
+	i0 = round(log(S0) / deltaX, 0) + I;
+    S0down = exp((i0 - I) * deltaX);
+    S0up = exp((i0 - I + 1) * deltaX);
+    downRatio = (S0up - S0) / (S0up - S0down);
+	OptVal = U(i0+1, 1) * (1 - downRatio) + U(i0, 1) * downRatio;
+	
+end
+
+function v = PSOR(a, b, c, initial_guess, Vplus, epsilon, omega, phi, Imax)
+    k = 0;
+    converged = false;
+    oldV = initial_guess;
+    newV = oldV;
+    while ~converged
+        psi = Vplus;
+        for i = 2 : Imax - 1
+            newV(i) = (psi(i) - a * newV(i-1) - c * oldV(i+1)) / b;
+            newV(i) = max(omega * newV(i) + (1 - omega) * oldV(i), phi(i));
+        end
+        if norm(newV - oldV) < epsilon
+            converged = true;
+        end
+        k = k + 1;
+        oldV = newV;
+    end
+    v = newV;
+end
--- Main/FD_ids_call_trans.m
+++ Main/FD_ids_call_trans.m
@@ -1,21 +1,27 @@
 function OptVal = FD_ids_call_trans(S0, X, r, q, T, sigma, I, N, xmax)
 	deltaT = T / N;
 	deltaX = xmax / I;
-	a = deltaT * (r - q - sigma ^ 2 / 2) / (2 * deltaX);
-	b = 1 + r * deltaT;
-	c = deltaT * (r - q) / (2 * deltaX);
+    alpha = sigma ^ 2 * deltaT / (deltaX ^ 2);
+    beta = 1 + r * deltaT;
+    gamma = deltaT * (r - q - sigma ^ 2 / 2) / (2 * deltaX);
+	a = gamma - alpha / 2;
+	b = beta + alpha;
+	c = -gamma - alpha / 2;
 	
-	U = max(exp(deltaX * (-I+1 : I-1) - X, 0);
+	U = transpose(max(exp(deltaX * (-I+1 : I-1)) - X, 0));
 	
-	%Generate A
+    A = full(gallery('tridiag', 2*I-1, a, b, c));
 	Ainverse = inv(A);
 	
 	for j = N-1 : -1 : 0
-		U(2I-1) = U(2I-1) + c * exp(-r*(N-j)*deltaT)*(exp(xmax) - X);
-		U = Ainverse * U;
+		U(2*I-1, 1) = U(2*I-1, 1) - c * exp(-r*(N-j)*deltaT)*(exp(xmax) - X);
+        U = Ainverse * U;
 	end
 	
-	i0 = round(log(S0) / deltaX, 0);
-	OptVal = U(i0);
+	i0 = round(log(S0) / deltaX, 0) + I;
+    S0down = exp((i0 - I) * deltaX);
+    S0up = exp((i0 - I + 1) * deltaX);
+    downRatio = (S0up - S0) / (S0up - S0down);
+	OptVal = U(i0+1, 1) * (1 - downRatio) + U(i0, 1) * downRatio;
 	
 end
