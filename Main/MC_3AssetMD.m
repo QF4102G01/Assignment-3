b2930e5a7f464eb4681d08823b33e913630a57ff
xmchenph <xmchenph@gmail.com>
2017-11-19 15:35:00 +0800

    MC_3AssetMD commenting

--- Main/MC_3AssetMD.m
+++ Main/MC_3AssetMD.m
@@ -8,21 +8,28 @@ function optionValue = MC_3AssetMD(S0, X, sigma, C, r, q, T, no_samples)
     initialRand = randn(3, no_samples);
     
     % Use Cholesky factorization to produce a lower triangular from the
-    % correlations matrix, since C is symmetric and positive definite.
-    L = chol(C)';
+    % correlations matrix, since C is positive definite.
+    L = chol(C);
     
-    % Generation of correlated 
-    correlatedRand = L * initialRand;
+    % Generation of correlated random variables
+    correlatedRand = initialRand * L;
     
+    % Initiate the basic variables for pricing
     mu = r - q - sigma .^ 2. / 2;
+    % Copy the values of sigma and expected price for no_samples times
     sigma_augmented = repmat(sigma, no_samples, 1);
     price_augmented = repmat(S0 .* exp(mu .* T), no_samples, 1);
     
+    % Terminal price simulated
     ST = price_augmented .* exp(sqrt(T) .* sigma_augmented .* correlatedRand);
-
+    
+    % Filter in only those samples that have the maximum price above strike
+    % price.
     max_prices = max(ST, [], 2);
     optionValues = (max_prices > X);
     
+    % Take present value of the mean of these option values, and obtain the
+    % final option value from the simulation.
     optionValue = mean(optionValues) * exp(-r * T);
     
 end
